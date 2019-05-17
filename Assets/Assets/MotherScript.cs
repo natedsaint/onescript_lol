b8395df389ba22fd88ddb55d3cd2b0370fe19a13
Nathan St. Pierre <nathan@unity3d.com>
2019-05-16 23:17:00 -0700

    lol why not

---
+++ Assets/Assets/MotherScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherScript : MonoBehaviour {
+  public GameObject baby1;
+  public GameObject baby2;
+
+  // Start is called before the first frame update
+  void Start() {
+    if (baby1 != null) {
+      baby1.AddComponent(typeof(BabyScript));
+      baby1.GetComponent<BabyScript>().babyName = "Bingo";
+    }
+    if (baby2 != null) {
+      baby2.AddComponent(typeof(BabyScript));
+      baby2.GetComponent<BabyScript>().babyName = "Bongo";
+    }
+  }
+
+  // Update is called once per frame
+  void Update() {
+      
+  }
+}
+
+public class BabyScript : MonoBehaviour {
+  public string babyName;
+  private void Start() {
+    Debug.Log(babyName + " Says: Waaaaahhh!");
+  }
+}
